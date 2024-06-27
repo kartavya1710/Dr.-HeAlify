2677bda2645c7827454f2185438bf5fd3fe6b070
kartavya1710 <57744964+kartavya1710@users.noreply.github.com>
2024-06-28 00:55:29 +0530

    app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if selected == 'Home':
     Thank you for choosing HeAlify Doctor Agent. Your health is our priority!
 
     **Contact Us:**
-    If you have any questions or need assistance, feel free to contact me at [CLICK ME](kartavyamaster17@gma).
+    If you have any questions or need assistance, feel free to contact me at [CLICK ME](mailto:kartavyamaster17@gmail.com).
     """)
     st.header('', divider='rainbow')
     with st.container(border=True): 
